b6a6174364187be31136b95acd52541dae9991ac
Linden Kyaw <31021147+tdslinden@users.noreply.github.com>
2024-12-06 14:34:58 -0800

    Remove eslint config

--- next.config.js
+++ next.config.js
@@ -7,10 +7,6 @@ const nextConfig = {
   },
   
   reactStrictMode: true,
-
-  eslint: {
-    dirs: ['pages', 'utils'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
-  },
 }
 
 module.exports = nextConfig
